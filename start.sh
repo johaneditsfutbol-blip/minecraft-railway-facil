c97f6cfe50564a1ba79fdc15c506d2d10c49ec8a
johaneditsfutbol-blip <johaneditsfutbol@gmail.com>
2025-12-25 17:35:40 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+ç#!/bin/bash
 
-echo "--- INICIANDO SISTEMA ---"
+echo "--- VERIFICANDO ARCHIVOS DEL SERVIDOR ---"
 
-# 1. Iniciar el Túnel Playit en segundo plano
-# Si no hay clave secreta, generará un link para reclamar.
+# ESTO ES LO NUEVO:
+# Si el archivo server.jar no existe (porque el volumen está vacío), lo descargamos aquí.
+if [ ! -f server.jar ]; then
+    echo "--- DESCARGANDO PAPERMC (Primera vez) ---"
+    curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.21/builds/130/downloads/paper-1.21-130.jar
+fi
+
+echo "--- INICIANDO SISTEMA DE TÚNEL ---"
+
+# Iniciar Playit
 if [ -z "$PLAYIT_SECRET" ]; then
     echo "NO HAY CLAVE DE TÚNEL. BUSCA EL LINK EN LOS LOGS PARA RECLAMAR."
     playit &
@@ -12,7 +20,9 @@ else
     playit --secret $PLAYIT_SECRET &
 fi
 
-# 2. Iniciar Servidor Minecraft
-# Le damos 2GB de RAM (ajustable a Railway)
+# Esperamos un poquito para asegurar que Playit arranque
+sleep 5
+
 echo "--- INICIANDO MINECRAFT ---"
+# Iniciamos el servidor
 java -Xms1G -Xmx2G -jar server.jar nogui
