a9499c27c9bc81b1273ffb10658f369782a73427
johaneditsfutbol-blip <johaneditsfutbol@gmail.com>
2025-12-25 17:37:46 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,29 @@
-ç#!/bin/bash
+#!/bin/bash
+
+echo "--- ACEPTANDO EULA ---"
+# Esto crea el archivo de licencia automáticamente para que no se apague
+echo "eula=true" > eula.txt
 
 echo "--- VERIFICANDO ARCHIVOS DEL SERVIDOR ---"
-
-# ESTO ES LO NUEVO:
-# Si el archivo server.jar no existe (porque el volumen está vacío), lo descargamos aquí.
+# Si el archivo server.jar no existe, lo descargamos
 if [ ! -f server.jar ]; then
-    echo "--- DESCARGANDO PAPERMC (Primera vez) ---"
+    echo "--- DESCARGANDO PAPERMC ---"
     curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.21/builds/130/downloads/paper-1.21-130.jar
 fi
 
 echo "--- INICIANDO SISTEMA DE TÚNEL ---"
-
-# Iniciar Playit
+# Iniciamos Playit en segundo plano
 if [ -z "$PLAYIT_SECRET" ]; then
-    echo "NO HAY CLAVE DE TÚNEL. BUSCA EL LINK EN LOS LOGS PARA RECLAMAR."
+    echo "NO HAY CLAVE. BUSCA EL LINK EN LOS LOGS."
     playit &
 else
     echo "USANDO CLAVE DE TÚNEL CONFIGURADA."
     playit --secret $PLAYIT_SECRET &
 fi
 
-# Esperamos un poquito para asegurar que Playit arranque
+# Esperamos 5 segundos
 sleep 5
 
 echo "--- INICIANDO MINECRAFT ---"
-# Iniciamos el servidor
+# Iniciamos el servidor con 2GB de RAM
 java -Xms1G -Xmx2G -jar server.jar nogui
